a44642fd59db1c999d16ef40f768e1f545d67a6a
Alex Sweeney <a.sweeney@stem.org.uk>
2023-10-17 10:27:48 +0100

    get tasks form db on page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
-import { allTasks } from '@/database/tasks'
+import { db } from '../database/db';
+import { tasks } from '../database/schema/tasks';
 
 export default function Home() { 
+  const allTasks = db.select().from(tasks).all();
   console.log('allTasks', allTasks)
 
   return (
--- src/database/tasks.ts
+++
@@ -1,4 +0,0 @@
-import { db } from './db';
-import { tasks } from './schema';
-
-export const allTasks = db.select().from(tasks).all();
